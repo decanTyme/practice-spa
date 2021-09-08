4cf07a09e25c9db39a6d3ef5d256748f0f59500d
Danry Ague <74456102+epcraft93@users.noreply.github.com>
2021-09-08 12:31:28 +0800

    [BUGFIX] Theme not changing

--- src/services/hooks/use-theme.js
+++ src/services/hooks/use-theme.js
@@ -4,7 +4,12 @@ import { Constants } from "../../constants";
 function useTheme() {
   const [theme, setTheme] = useState(Constants.DEFAULT_THEME);
 
-  const changeTheme = (theme) => setTheme(theme);
+  const changeTheme = (theme) =>
+    setTheme(
+      theme === Constants.DEFAULT_THEME
+        ? Constants.THEME_DARK
+        : Constants.THEME_LIGHT
+    );
 
   return { theme, changeTheme };
 }
