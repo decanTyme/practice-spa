32b6a18d2256b67c7d57e6bc16c90619fded4754
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2021-09-16 22:31:15 +0800

    Initial implementation of new Redux-based Data Service

---
+++ src/app/state/reducers/data/index.js
@@ -0,0 +1,108 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import HttpService from "../../../../services/http";
+
+export const fetchData = createAsyncThunk("dataService/fetch", async () => {
+  const response = await HttpService().onDataFetch();
+
+  return response.data;
+});
+
+export const pushData = createAsyncThunk("dataService/push", async (data) => {
+  const response = await HttpService().onDataPush(data);
+
+  return response.data;
+});
+
+export const updateData = createAsyncThunk(
+  "dataService/modify",
+  async (modifiedData) => {
+    const response = await HttpService().onDataModify(modifiedData);
+
+    if (response.data.success) return modifiedData;
+  }
+);
+
+export const removeData = createAsyncThunk("dataService/delete", async (id) => {
+  const response = await HttpService().onDataRemove(id);
+
+  if (response.data.success) return id;
+});
+
+const slice = createSlice({
+  name: "dataService",
+  initialState: {
+    data: [],
+    status: "idle",
+    error: null,
+    dataDetails: null,
+    currentlyModifying: null,
+  },
+  reducers: {
+    viewData: (state, action) => {
+      state.dataDetails = action.payload;
+
+      state.currentlyModifying = null;
+    },
+
+    modifyData: (state, action) => {
+      state.currentlyModifying = action.payload;
+    },
+
+    deleteAllData: (state) => {
+      state.data = [];
+      state.status = "idle";
+      state.error = null;
+      state.dataDetails = null;
+      state.currentlyModifying = null;
+    },
+  },
+
+  extraReducers: (builder) => {
+    /* Fetch data cases */
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.status = "success";
+        state.data = action.payload;
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+
+    /* Push data cases */
+    builder.addCase(pushData.fulfilled, (state, action) => {
+      state.data.push(action.payload.product);
+    });
+
+    /* Update data cases */
+    builder.addCase(updateData.fulfilled, (state, action) => {
+      state.data = state.data.filter(({ _id }) => _id !== action.payload._id);
+      state.data.push(action.payload);
+
+      state.currentlyModifying = null;
+      state.dataDetails = action.payload;
+    });
+
+    /* Remove data cases */
+    builder.addCase(removeData.fulfilled, (state, action) => {
+      state.data = state.data.filter(({ _id }) => _id !== action.payload);
+
+      state.currentlyModifying = null;
+      state.dataDetails = null;
+    });
+  },
+});
+
+export const { viewData, modifyData, deleteAllData } = slice.actions;
+
+// State data selectors
+export const selectData = (state) => state.root.data.data;
+export const selectDataDetails = (state) => state.root.data.dataDetails;
+export const selectDataInEdit = (state) => state.root.data.currentlyModifying;
+export const selectDataError = (state) => state.root.data.error;
+export const selectDataStatus = (state) => state.root.data.status;
+
+export default slice.reducer;
--- src/routes/home/pages/shop/index.jsx
+++ src/routes/home/pages/shop/index.jsx
@@ -1,34 +1,31 @@
 import { Route, Switch, Redirect, useRouteMatch } from "react-router-dom";
 import Soon from "../../../ComingSoon";
-import DataService from "../../../components/DataService";
 import Products from "./products/wrapper";
 
 function Shop() {
   const { path } = useRouteMatch();
 
   return (
-    <DataService>
-      <Switch>
-        <Route path={`${path}/products`}>
-          <Products />
-        </Route>
-        <Route path={`${path}/orders`}>
-          <Soon />
-        </Route>
-        <Route path={`${path}/customers`}>
-          <Soon />
-        </Route>
+    <Switch>
+      <Route path={`${path}/products`}>
+        <Products />
+      </Route>
+      <Route path={`${path}/orders`}>
+        <Soon />
+      </Route>
+      <Route path={`${path}/customers`}>
+        <Soon />
+      </Route>
 
-        {/* ------------------------- 404 Not Found ------------------------- */}
-        <Route
-          component={({ location }) => (
-            <Redirect
-              to={Object.assign({}, location, { state: { is404: true } })}
-            />
-          )}
-        />
-      </Switch>
-    </DataService>
+      {/* ------------------------- 404 Not Found ------------------------- */}
+      <Route
+        component={({ location }) => (
+          <Redirect
+            to={Object.assign({}, location, { state: { is404: true } })}
+          />
+        )}
+      />
+    </Switch>
   );
 }
 
