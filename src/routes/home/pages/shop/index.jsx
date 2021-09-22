2459dd9c89818509ad7a9ccdb6ada28fa0dccfee
Danry Ague <74456102+epcraft93@users.noreply.github.com>
2021-09-22 15:23:27 +0800

    Directory restructure

--- src/routes/home/pages/shop/index.jsx
+++ src/routes/home/pages/shop/index.jsx
@@ -1,6 +1,7 @@
 import { Route, Switch, Redirect, useRouteMatch } from "react-router-dom";
 import Soon from "../../../ComingSoon";
-import Products from "./products/ProductsWrapper";
+import Orders from "./orders";
+import Products from "./products";
 
 function Shop() {
   const { path } = useRouteMatch();
@@ -11,7 +12,7 @@ function Shop() {
         <Products />
       </Route>
       <Route path={`${path}/orders`}>
-        <Soon />
+        <Orders />
       </Route>
       <Route path={`${path}/customers`}>
         <Soon />
