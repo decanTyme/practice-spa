4a51268dee23f5effaba42117c049a012dde00ed
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2021-09-25 12:07:57 +0800

    Add support for getting specific subdata arrays from the entire datastore
    Add data transformation phases to ensure proper structure
    
    Update modify thunk
    
    Update product schema

--- src/app/state/slices/data/product/selectors.js
+++ src/app/state/slices/data/product/selectors.js
@@ -1,5 +1,17 @@
 export const selectAllProducts = (state) =>
-  state.root.DataService.products.data;
+  state.root.DataService.products.data.all;
+
+export const selectAllBrands = (state) =>
+  state.root.DataService.products.data.brands;
+
+export const selectAllClasses = (state) =>
+  state.root.DataService.products.data.classes;
+
+export const selectAllCategories = (state) =>
+  state.root.DataService.products.data.categories;
+
+export const selectAllUnits = (state) =>
+  state.root.DataService.products.data.units;
 
 export const selectProductDetails = (state) =>
   state.root.DataService.products.details;
@@ -16,14 +28,26 @@ export const selectProductImportedCSV = (state) =>
 export const selectCurrentlySelectedProducts = (state) =>
   state.root.DataService.products.currentlySelected;
 
+/* Fetch */
 export const selectProductFetchStatus = (state) =>
   state.root.DataService.products.status.fetch;
 
 export const selectProductFetchError = (state) =>
   state.root.DataService.products.error.fetch;
 
+/* Push */
 export const selectProductPushStatus = (state) =>
   state.root.DataService.products.status.push;
 
-export const selectProductError = (state) =>
+export const selectProductPushError = (state) =>
+  state.root.DataService.products.error.push;
+
+/* Modify */
+export const selectProductModifyStatus = (state) =>
+  state.root.DataService.products.status.modify;
+
+export const selectProductModifyError = (state) =>
+  state.root.DataService.products.error.modify;
+
+export const selectAllProductError = (state) =>
   state.root.DataService.products.error;
