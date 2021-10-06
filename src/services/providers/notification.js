02704609fefbf6ab2b824bdd5bb64728360693e2
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2021-10-06 23:07:10 +0800

    Remove unused file

--- src/services/providers/notification.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const NotificationContext = createContext();
-
-function useNotifyService() {
-  return useContext(NotificationContext);
-}
-
-export default useNotifyService;
