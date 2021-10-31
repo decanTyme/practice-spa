75846ea250fbef5de7936503519a9d4d83eacaa3
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2021-10-31 15:34:24 +0800

    [BUGFIX] Render-persistent ghost tooltips

--- src/services/hooks/use-init-tooltips.js
+++ src/services/hooks/use-init-tooltips.js
@@ -6,12 +6,41 @@ function useInitializeTooltips({
   html = false,
   customClass = "",
   offset = [0, 0],
+  trigger = "hover focus",
+  template = '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',
 } = {}) {
   useEffect(() => {
-    document
-      .querySelectorAll('[data-bs-toggle="tooltip"]')
-      .forEach((tooltipTrigger) => {
-        new Tooltip(tooltipTrigger, { animation, html, customClass, offset });
+    const tooltipList = [
+      ...document.querySelectorAll(`[data-bs-toggle="tooltip"]`),
+    ].map(
+      (tooltipTriggerElement) =>
+        new Tooltip(tooltipTriggerElement, {
+          animation,
+          html,
+          customClass,
+          offset,
+          trigger,
+          template,
+        })
+    );
+
+    // Dispose all initialized tooltips on component unmount to prevent a
+    // "ghost" tooltip that persists even on re-renders. These bugged
+    // tooltips can only be removed via full page reloads.
+    //
+    // However, disposing the tooltips sometimes cause the error:
+    //! Uncaught TypeError: Cannot read properties of null (reading 'template')
+    //
+    // Hence, more info is needed for a complete fix.
+    return () =>
+      tooltipList.forEach((tooltip) => {
+        try {
+          tooltip._config && tooltip.dispose();
+        } catch (error) {
+          // Log so more information
+          // on the bug is noted
+          console.log(error, tooltip);
+        }
       });
   });
 }
